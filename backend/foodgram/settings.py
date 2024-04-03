bb4ab7d95522e77ebef5f336432a0bdf528944f9
Nick Kobzar <nikita.shikari@gmail.com>
2024-04-03 21:20:01 +0300

    final commit probably

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,11 +11,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-
+CSRF_TRUSTED_ORIGINS = ['https://bokuva.duckdns.org']
 # Application definition
 
 INSTALLED_APPS = [
